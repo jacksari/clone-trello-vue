d8b5d318f7dbcd1f5ad9203764ce44cd307c8a9e
Jack Sari <jack.sanchez@alarcongroup.us>
2022-09-09 15:23:55 -0500

    start commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<script setup lang="ts">
+  // const store = counter
+</script>
+
+<template>
+  <div>
+    <h1 className="text-xl font-bold ">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
